af8918fcab941d6bab3632f5ebd11440b7ed48ae
mong3125 <mong3125@naver.com>
2022-09-15 21:43:49 +0900

    initial commit

---
+++ src/main/java/com/example/wineapi/WineApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.wineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineApiApplication.class, args);
+	}
+
+}
