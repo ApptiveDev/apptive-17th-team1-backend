07d7307063c8e094db1ae144a5b827ca69dd3a50
JeongHoHwang <wjdgh721224@naver.com>
2022-09-26 14:15:08 +0900

    ContainerController

--- src/main/java/com/example/wineapi/data/dto/ContainerDTO.java
+++ src/main/java/com/example/wineapi/data/dto/ContainerDTO.java
@@ -1,28 +1,16 @@
-package com.example.wineapi.data.entity;
+package com.example.wineapi.data.dto;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name = "Container")
-public class Container {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
+public class ContainerDTO {
     private Long user_id;
-
-    @Column(nullable = false)
     private Long wine_id;
-
     private boolean is_like;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
+
+    public ContainerDTO(Long user_id, Long wine_id, boolean is_like) {
+        this.user_id = user_id;
+        this.wine_id = wine_id;
+        this.is_like = is_like;
     }
 
     public Long getUser_id() {
@@ -51,9 +39,8 @@ public class Container {
 
     @Override
     public String toString() {
-        return "Container{" +
-                "id=" + id +
-                ", user_id=" + user_id +
+        return "ContainerDTO{" +
+                "user_id=" + user_id +
                 ", wine_id=" + wine_id +
                 ", is_like=" + is_like +
                 '}';
