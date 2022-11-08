0923198ec1993e48e4ea7c99cfce953ccdc49b4e
mong3125 <mong3125@naver.com>
2022-11-08 13:20:53 +0900

    question api string출력에서 integer출력으로

--- src/main/java/com/example/wineapi/controller/QuestionController.java
+++ src/main/java/com/example/wineapi/controller/QuestionController.java
@@ -19,19 +19,6 @@ public class QuestionController {
         this.questionService = questionService;
     }
 
-    @RequestMapping(value = "/questionn/{id}", method = RequestMethod.GET)
-    public ResponseEntity<QuestionDto> QuestionApi(@PathVariable Integer id) {
-
-        QuestionDto result = questionService.JsonQuestionById(id);
-
-//        // null 값일때 404반환
-//        if (result.getId() == null) {
-//            return ResponseEntity.notFound().build();
-//        }
-
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
-
     @RequestMapping(value = "/question/category/{category}", method = RequestMethod.GET)
     public ResponseEntity<ArrayList<QuestionDto>> QuestionByCategory(@PathVariable("category") Integer category) {
         ArrayList<QuestionDto> result = questionService.QuestionDtoByCategory(category);
