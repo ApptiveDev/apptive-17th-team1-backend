23e3bf40ea377ad4e016877f580de56542033073
Daegil Kim <mong3125@naver.com>
2022-11-29 16:05:26 +0900

    refactor: exception package 추가
    추후 controller에 exception을 통한 error status 출력으로 수정해야함

---
+++ src/main/java/com/example/wineapi/global/error/exception/NotFoundImageException.java
@@ -0,0 +1,23 @@
+package com.example.wineapi.global.error.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Image Not Found")
+public class NotFoundImageException extends Exception{
+
+    private final int ERROR_CODE;
+
+    public NotFoundImageException(int ERROR_CODE) {
+        super("이미지를 찾을 수 없습니다");
+        this.ERROR_CODE = ERROR_CODE;
+    }
+
+    public NotFoundImageException() {
+        this(404);
+    }
+
+    public int getERROR_CODE() {
+        return ERROR_CODE;
+    }
+}
