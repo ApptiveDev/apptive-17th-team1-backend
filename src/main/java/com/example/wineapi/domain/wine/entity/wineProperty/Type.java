40fa7d4cb2b02cd1976a7cadf4cd194c798eb156
Daegil Kim <mong3125@naver.com>
2022-12-27 23:42:49 +0900

    feature: wine property entity

---
+++ src/main/java/com/example/wineapi/domain/wine/entity/wineProperty/Type.java
@@ -0,0 +1,19 @@
+package com.example.wineapi.domain.wine.entity.wineProperty;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Type{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    private String content;
+}
